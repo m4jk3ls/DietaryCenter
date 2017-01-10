6460de9962c07f236714e397f4811380df7f4190
Michal Sroka <m4jk3ls@gmail.com>
2017-01-10 19:15:18 +0100

    Renaming

---
+++ nowa_twoja_karta.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<title>Twoja karta pacjenta</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<link rel="stylesheet" href="nowa_twoja_karta_style.css" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Great+Vibes|Playfair+Display:400,700&amp;subset=latin-ext" rel="stylesheet">
+</head>
+
+<body>
+	<div id="container">
+		<div id="logo">
+			<img src="logo.jpg" />
+		</div>
+
+		<div id="menu">
+			<div class="option">Strona główna</div>
+			<div class="option">Twoja wizyta</div>
+			<div class="option">Osiągnięte rezultaty</div>
+			<div class="option">Kontakt</div>
+			<div id="lastOption">Wyloguj</div>
+			<div style="clear:both;"></div>
+		</div>
+
+		<div id="topbar">
+			<div id="topbarL">
+				<img src="czlowiek-warzywko.jpg" />
+			</div>
+
+			<div id="topbarR">
+				<div id="cytat">
+					"Granice? Nigdy żadnej nie widziałem, ale słyszałem, że istnieją w umysłach niektórych ludzi".
+				</div>
+
+				<div id="podpis">
+					Thor Heyerdahl
+				</div>
+			</div>
+
+			<div style="clear: both;"></div>
+		</div>
+	</div>
+</body>
+</html>
--- twoja_karta.php
+++ twoja_karta.php
@@ -1,45 +1,69 @@
-<!DOCTYPE html>
+<?php
+	session_start();
+
+	if(!isset($_COOKIE["zalogowany"]))
+	{
+		header('Location: index.php');
+		exit();
+	}
+	else
+	{
+		require_once "connect.php";
+		mysqli_report(MYSQLI_REPORT_STRICT);
+		try
+		{
+			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+			$polaczenie->set_charset('utf8');
+
+			if ($polaczenie->connect_errno != 0)
+				throw new Exception($polaczenie->connect_error);
+			else
+			{
+				$login_help = $_SESSION['login'];
+				$token_help = $_COOKIE['token'];
+
+				if (!($rezultat = $polaczenie->query("select count(Token) from aktywne_sesje where Login like '$login_help' and Token like '$token_help'")))
+					throw new Exception($polaczenie->error);
+				if(!$rezultat->fetch_assoc()['count(Token)'])	//Jesli w bazie nie ma pasujacego do ciastka token'a (zostal zwrocony wiersz z wartoscia count(Token)=0)
+				{
+					if(!$polaczenie->query("delete from aktywne_sesje where Login like '$login_help'"))
+						throw new Exception($polaczenie->error);
+
+					$polaczenie->close();
+					$rezultat->free_result();
+					header('Location: logout.php');
+					exit();
+				}
+
+				$rezultat->free_result();
+				$polaczenie->close();
+			}
+		}
+		catch(Exception $e)
+		{
+			echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o ponowne zalogowanie się!</span>';
+			//echo '<br/>Informacja developerska: '.$e;
+			sleep(5);
+			header('Location: logout.php');
+			exit();
+		}
+	}
+?>
+
+<!DOCTYPE HTML>
 <html lang="pl">
 <head>
-	<meta charset="utf-8">
-	<title>Twoja karta pacjenta</title>
+	<meta charset="utf-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-	<link rel="stylesheet" href="twoja_karta_styl.css" type="text/css" />
-	<link href="https://fonts.googleapis.com/css?family=Great+Vibes|Playfair+Display:400,700&amp;subset=latin-ext" rel="stylesheet">
+	<title>NaturHouse - Twoja karta pacjenta</title>
 </head>
 
 <body>
-	<div id="container">
-		<div id="logo">
-			<img src="logo.jpg" />
-		</div>
-
-		<div id="menu">
-			<div class="option">Strona główna</div>
-			<div class="option">Twoja wizyta</div>
-			<div class="option">Osiągnięte rezultaty</div>
-			<div class="option">Kontakt</div>
-			<div id="lastOption">Wyloguj</div>
-			<div style="clear:both;"></div>
-		</div>
-
-		<div id="topbar">
-			<div id="topbarL">
-				<img src="czlowiek-warzywko.jpg" />
-			</div>
-
-			<div id="topbarR">
-				<div id="cytat">
-					"Granice? Nigdy żadnej nie widziałem, ale słyszałem, że istnieją w umysłach niektórych ludzi".
-				</div>
-
-				<div id="podpis">
-					Thor Heyerdahl
-				</div>
-			</div>
-
-			<div style="clear: both;"></div>
-		</div>
-	</div>
+<?php
+	echo "<p>Witaj ".$_SESSION['login']." - jesteś zalogowany do swojego NH-konta".'! [<a href="logout.php">Wyloguj się</a>]</p>';
+	
+	$dataCzas = new DateTime();
+	echo "Mamy dziś ".$dataCzas->format('Y-m-d').", godzinę ".$dataCzas->format('H:i:s')."<br/>";
+?>
 </body>
 </html>
