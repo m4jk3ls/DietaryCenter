569f67f0aa731978ebcc5babd1383c271932271d
Michal Sroka <m4jk3ls@gmail.com>
2017-01-29 15:21:10 +0100

    Main page (after log in) looks nice, but it must be do so much better (e.g. with Zelent - CSS3 section #3)

--- nowa_twoja_karta.php
+++ nowa_twoja_karta.php
@@ -40,6 +40,28 @@
 
 			<div style="clear: both;"></div>
 		</div>
+
+		<div id="sidebar">
+			<div class="optionL">Strona główna</div>
+			<div class="optionL">Twoja wizyta</div>
+			<div class="optionL">Osiągnięte rezultaty</div>
+			<div class="optionL">Kontakt</div>
+			<div class="optionL">Wyloguj</div>
+		</div>
+
+		<div id="content">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula dolor dapibus diam consequat, vel bibendum tortor pulvinar. Aenean eleifend odio quis ligula congue, sit amet suscipit orci placerat. Phasellus non nisl sit amet mauris tincidunt finibus. Curabitur convallis rutrum tempor. Vivamus venenatis, est id imperdiet pharetra, ligula mauris maximus lorem, at finibus neque dui malesuada lorem. Quisque aliquam massa id nulla consectetur laoreet. Aenean egestas, quam vitae fermentum placerat, neque urna consequat tellus, a finibus sem lacus non odio. Donec venenatis facilisis suscipit. Aenean quis gravida erat.
+
+			In non ipsum sed nisl efficitur tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque at magna a ligula mollis condimentum id sit amet sem. Ut dictum orci augue. Sed a condimentum orci. Integer suscipit bibendum magna, eu sodales dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent varius id sem a accumsan. Duis sit amet lectus in tortor faucibus ornare. Nullam in odio luctus, luctus tellus a, viverra magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent et viverra diam, a feugiat ex. Aenean ac ante nunc. Fusce tempus, felis a feugiat dictum, mauris nisl sollicitudin turpis, non sodales orci elit id lorem.
+
+			Donec vulputate felis ac blandit mollis. Donec maximus, erat quis ullamcorper bibendum, lorem est luctus risus, ac euismod justo enim vel augue. Praesent vehicula, ligula in posuere pellentesque, ipsum turpis mollis arcu, ac commodo mi tortor sit amet libero. Nullam vehicula lacus at auctor ullamcorper. Morbi nec turpis scelerisque, dapibus lorem ut, egestas dolor. Nam vitae elit felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+			Proin pulvinar accumsan efficitur. Quisque egestas lorem nec mi condimentum vulputate. Aliquam tempor, elit sit amet placerat imperdiet, nunc eros pharetra lorem, in sodales urna dolor at libero. Suspendisse scelerisque, nibh eu lacinia mattis, ligula massa volutpat nunc, sit amet luctus massa lectus sit amet nibh. Maecenas pulvinar facilisis nisi. Ut laoreet faucibus urna eu facilisis. In cursus vitae leo tempor dictum. Integer nec sagittis nibh. Pellentesque iaculis, ex quis condimentum blandit, ex nisi aliquam enim, a fermentum felis mi eget tortor. Nullam magna lacus, luctus ut lectus eget, efficitur consequat augue. Etiam nisl ipsum, dignissim vitae lorem sed, ultrices viverra ipsum. Sed id laoreet enim.
+		</div>
+
+		<div id="footer">
+			NaturHouse - Twój osobisty dietetyk. Strona w sieci od 2017 r. &copy; Wszelkie prawa zastrzeżone
+		</div>
 	</div>
 </body>
 </html>
