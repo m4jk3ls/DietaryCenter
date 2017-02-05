f06b082b46f0a8ac882077cb3ba88a183adc7894
Michal Sroka <m4jk3ls@gmail.com>
2017-02-05 22:45:15 +0100

    New layout with CSS3

--- index.php
+++ index.php
@@ -15,17 +15,27 @@
 	<meta name="keywords" content="naturhouse, dietetyk, najlepszy dietetyk, dieta cud, jak schudnąć do wakacji, jak być zdrowym, zdrowe odżywianie, plan dietetyczny, organizacja posiłków, metamorfoza"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 	<title>NaturHouse - dietetyk na wyciągnięcie ręki</title>
+	<link rel="stylesheet" href="index_style.css" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Great+Vibes|Playfair+Display:400,700&amp;subset=latin-ext" rel="stylesheet">
 </head>
 
 <body>
-	<a href="rejestracja.php">Zarejestruj się i dołącz do nas!</a>
-	<br/><br/>
+	<div id="container">
+		<div id="header">Witaj w świecie NaturHouse!</div>
 
-	<form action="zaloguj.php" method="post">
-		Login: <br/> <input type="text" name="login"/> <br/>
-		Hasło: <br/> <input type="password" name="haslo"/> <br/> <br/>
-		<input type="submit" value="Zaloguj się"/>
-	</form>
+		<div id="log_form">
+
+			<form action="zaloguj.php" method="post">
+				<input type="text" name="login" placeholder="login"/> <br/>
+				<input type="password" name="haslo" placeholder="hasło"/> <br/> <br/>
+				<input type="submit" value="Zaloguj się"/>
+			</form>
+
+			<div id="tekst_lub">-------- lub --------</div>
+			<div id="link_rejestracji"><a href="rejestracja.php">Zarejestruj się i dołącz do nas!</a></div>
+
+		</div>
+	</div>
 <?php
 	if(isset($_SESSION['blad']))	echo $_SESSION['blad'];
 ?>
