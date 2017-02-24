e6841c7d65ddf430e56f8fed9881a3316f994c33
Michal Sroka <m4jk3ls@gmail.com>
2017-02-24 22:52:35 +0100

    Nothing changed

--- yourCard.php
+++ yourCard.php
@@ -73,7 +73,7 @@ else
 		<li><a href="yourCard.php">Strona główna</a></li>
 		<li><a href="#">Twoja wizyta</a></li>
 		<li><a href="#">Twoje rezultaty</a></li>
-		<li><a href="#">Kontakt</a></li>
+		<li><a href="contact.php">Kontakt</a></li>
 		<li><a href="logOut.php">Wyloguj</a></li>
 	</ol>
 	<div id="topbarPackage">
