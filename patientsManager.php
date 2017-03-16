8b47bd9846368167c963533d8d4efe63c91e4f02
Michal Sroka <m4jk3ls@gmail.com>
2017-03-16 14:32:23 +0100

    Basics parts of subpage

--- patientsManager.php
+++ patientsManager.php
@@ -1,7 +1,75 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Michal
- * Date: 2017-03-16
- * Time: 13:28
- */
+session_start();
+
+if(!isset($_COOKIE['adminLogged']))
+{
+	header("Location: index.php");
+	exit();
+}
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<title>Panel admina NaturHouse</title>
+	<link rel="stylesheet" href="css_files/basic.css" type="text/css"/>
+	<link href="css_files/card.css" rel="stylesheet" type="text/css"/>
+	<link href="https://fonts.googleapis.com/css?family=Great+Vibes|Playfair+Display:400,700&amp;subset=latin-ext"
+		  rel="stylesheet">
+	<script src="javascript_files/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="javascript_files/stickyMenu.js"></script>
+	<link rel="stylesheet" type="text/css"
+		  href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+	<script src="javascript_files/cookiesBanner.js"></script>
+	<noscript><div id="infoAboutNoScript">Twoja przeglądarka nie obsługuje skryptów JavaScript!</div></noscript>
+	<style>
+		.menu > li:first-child
+		{
+			width: 25%;
+		}
+
+		.menu > li:first-child + li
+		{
+			width: 25%;
+		}
+
+		.menu > li:first-child + li + li
+		{
+			width: 25%;
+		}
+
+		.menu > li:first-child + li + li + li
+		{
+			width: 25%;
+		}
+	</style>
+</head>
+
+<body>
+<div id="container">
+	<div id="logo"><img id="logo-img" src="img/logo.jpg"/></div>
+	<ol class="menu">
+		<li><a href="adminPanel.php">Strona główna</a></li>
+		<li><a href="dieticiansManager.php">Dietetycy</a></li>
+		<li><a href="patientsManager.php">Pacjenci</a></li>
+		<li><a href="logOut.php">Wyloguj</a></li>
+	</ol>
+	<div id="topbarPackage">
+		<div id="topbar">
+			<div id="topbarL"><img id="topbarL-img" src="img/admin.png"/></div>
+			<div id="topbarR">
+				<div id="quotation">„Jesteśmy tym, co wciąż powtarzamy. Doskonałość nie jest zatem aktem, ale nawykiem".</div>
+				<div id="signature">Arystoteles</div>
+			</div>
+		</div>
+	</div>
+	<div id="content">
+	</div>
+	<div id="footer">NaturHouse - Twój osobisty dietetyk. Strona w sieci od 2017 r. &copy;
+					 Wszelkie prawa zastrzeżone</div>
+</div>
+</body>
+</html>
